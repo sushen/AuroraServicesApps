b7a685f3dea4fddc09dbab22792285a4b02d2d3c
Manishankar Vakta <evilrider555@gmail.com>
2019-01-06 11:44:24 +0600

    first

---
+++ app/src/main/java/com/services/auroratec/auroraservicesapps/MainActivity.java
@@ -0,0 +1,54 @@
+package com.services.auroratec.auroraservicesapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create Web View
+        WebView webb = (WebView)findViewById(R.id.webview1);
+        webb.getSettings().setJavaScriptEnabled(true);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("https://services.auroratec.net");
+
+        //Hook back key with application
+        webb.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+
+}
